155d12981ba54cfa0ab32da7a0181916216154ae
Ivan Wang <xLivfe.iw@gmail.com>
2024-06-06 20:30:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,19 +17,9 @@
 
 
 # Add a feed source
-sed -i "/helloworld/d" "feeds.conf.default"
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+# sed -i "/helloworld/d" "feeds.conf.default"
+# echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 # sed -i "/passwall/d" "feeds.conf.default"
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a 
-sed -i "/MosDSN/d" "feeds.conf.default"
-# echo 'src-git MosDSN https://github.com/sbwml/luci-app-mosdns.git' >>feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
